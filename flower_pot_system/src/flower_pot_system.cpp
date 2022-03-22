6aba553ff039ff5848cf84100c0bda2e92af2184
DennisDavi <ddavis111@cnm.edu>
2022-03-22 11:57:06 -0600

    published to the IO

--- flower_pot_system/src/flower_pot_system.cpp
+++ flower_pot_system/src/flower_pot_system.cpp
@@ -12,17 +12,26 @@
  */
 
 #include "Adafruit_BME280.h"
-#include "math.h"
 #include "Adafruit_MQTT_SPARK.h"
-#include "Adafruit_MQTT.h"
+#include "math.h"
+
+#include "Adafruit_MQTT/Adafruit_MQTT.h"
+#include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h"
 #include "credentials.h"
 
 void setup();
 void loop();
-#line 14 "c:/Users/DennisDavis/Documents/IoT/flowerPot-2nd-midterm/flower_pot_system/src/flower_pot_system.ino"
+void MQTT_connect();
+#line 16 "c:/Users/DennisDavis/Documents/IoT/flowerPot-2nd-midterm/flower_pot_system/src/flower_pot_system.ino"
 TCPClient theClient;
 
-Adafruit_MQTT_SPARK mqtt(&theClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY);
+Adafruit_MQTT_SPARK mqtt(&theClient, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+Adafruit_MQTT_Publish mqttObj1 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/SoilReading");
+Adafruit_MQTT_Publish mqttObj2 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/AirQuality");
+Adafruit_MQTT_Publish mqttObj3 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/DustReading");
+Adafruit_MQTT_Publish mqttObj4 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/TemperatureReading");
+Adafruit_MQTT_Publish mqttObj5 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/HumidityReading");
 
 Adafruit_MQTT_Subscribe mqttObj0 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/PumpButton");
 
@@ -32,9 +41,9 @@ int button1 = A1;
 bool buttonState;
 int oldButton;
 int OnOff;
-int soilMeter=A2;
+int soilMeter = A2;
 int soilRead;
-int airSensor=A3;
+int airSensor = A3;
 int airQuality;
 int dustSensor = A4;
 int dustReading;
@@ -48,60 +57,54 @@ int hexAddress = 0x76;
 unsigned long last, lastTime;
 int value1, value2;
 
-
-
-//SYSTEM_MODE(SEMI_AUTOMATIC)
-
+// SYSTEM_MODE(SEMI_AUTOMATIC)
 
 void setup() {
-  Serial.begin(9600);
-  status = bme.begin(hexAddress);
-  if (status == false){
-    Serial.printf("BME280 at address 0x%02X failed to start", hexAddress);
-  }
+    Serial.begin(9600);
+    status = bme.begin(hexAddress);
+    if (status == false) {
+        Serial.printf("BME280 at address 0x%02X failed to start", hexAddress);
+    }
     pinMode(button1, INPUT_PULLDOWN);
-    pinMode(motor,OUTPUT);
-    pinMode(soilMeter,INPUT);
+    pinMode(motor, OUTPUT);
+    pinMode(soilMeter, INPUT);
 
     waitFor(Serial.isConnected, 15000);
 
     WiFi.connect();
-    while(WiFi.connecting()){
-      Serial.printf(".");
+    while (WiFi.connecting()) {
+        Serial.printf(".");
     }
 
     mqtt.subscribe(&mqttObj0);
-
 }
 
 void loop() {
-  //MQTT_connect();
+    MQTT_connect();
 
-  if ((millis()-last)>120000){
-    Serial.printf("Pinging MQTT \n");
-    if(! mqtt.ping()){
-      Serial.printf("Disconnecting \n");
-      mqtt.disconnect();
+    if ((millis() - last) > 120000) {
+        Serial.printf("Pinging MQTT \n");
+        if (!mqtt.ping()) {
+            Serial.printf("Disconnecting \n");
+            mqtt.disconnect();
+        }
+        last = millis();
     }
-    last=millis();
-  }
 
-  Adafruit_MQTT_Subscribe *subscription;
-  while((subscription=mqtt.readSubscription(100))){
-    if(subscription == &mqttObj0){
-      value2 = atof((char *)mqttObj0.lastread);
+    Adafruit_MQTT_Subscribe *subscription;
+    while ((subscription = mqtt.readSubscription(100))) {
+        if (subscription == &mqttObj0) {
+            value2 = atof((char *)mqttObj0.lastread);
+        }
     }
-    
-    }
-    
-  
-  tempC = bme.readTemperature();
-  pres = bme.readPressure();
-  hum = bme.readHumidity();
 
-  tempF = tempC * (9.0/5.0)+32.2;
+    tempC = bme.readTemperature();
+    pres = bme.readPressure();
+    hum = bme.readHumidity();
 
-  buttonState = digitalRead(button1);
+    tempF = tempC * (9.0 / 5.0) + 32.2;
+
+    buttonState = digitalRead(button1);
     if (buttonState != oldButton) { // turning the button on and off
         if (buttonState == true) {
             OnOff = !OnOff;
@@ -109,15 +112,45 @@ void loop() {
         }
         oldButton = buttonState;
     }
-    if(OnOff == HIGH || value2 == HIGH){
-    digitalWrite(motor,HIGH);
-    }else{
-      digitalWrite(motor,LOW);
+    if (OnOff == HIGH || value2 == HIGH) {
+        digitalWrite(motor, HIGH);
+    } else {
+        digitalWrite(motor, LOW);
     }
 
-    soilRead=analogRead(soilMeter);
-    airQuality=analogRead(airSensor);
-    dustReading=analogRead(dustSensor);
-    Serial.printf("soil reading:%i air reading:%i dust reading: %i Temp is:%0.2f Humidity:%0.2f\n",soilRead, airQuality, dustReading, tempF, hum);
+    soilRead = analogRead(soilMeter);
+    airQuality = analogRead(airSensor);
+    dustReading = analogRead(dustSensor);
+    Serial.printf("soil reading:%i air reading:%i dust reading: %i Temp is:%0.2f Humidity:%0.2f\n", soilRead, airQuality, dustReading, tempF, hum);
 
+    if ((millis() - lastTime > 30000)) {
+        if (mqtt.Update()) {
+            mqttObj1.publish(soilRead);
+            mqttObj2.publish(airQuality);
+            mqttObj3.publish(dustReading);
+            mqttObj4.publish(tempF);
+            mqttObj5.publish(hum);
+            Serial.printf("soil reading:%i air reading:%i dust reading: %i Temp is:%0.2f Humidity:%0.2f\n", soilRead, airQuality, dustReading, tempF, hum);
+        }
+        lastTime = millis();
+    }
+}
+
+void MQTT_connect() {
+    int8_t ret;
+
+    // Stop if already connected.
+    if (mqtt.connected()) {
+        return;
+    }
+
+    Serial.print("Connecting to MQTT... ");
+
+    while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+        Serial.printf("%s\n", (char *)mqtt.connectErrorString(ret));
+        Serial.printf("Retrying MQTT connection in 5 seconds..\n");
+        mqtt.disconnect();
+        delay(5000); // wait 5 seconds
+    }
+    Serial.printf("MQTT Connected!\n");
 }
